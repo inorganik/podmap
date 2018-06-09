6e4b78030dd59fa5a5b08276343c8e789221f5fc
Jamie Perkins <jamieperkins@Computa.local>
2018-06-08 18:52:41 -0600

    moderation list WIP

--- src/app/podmap/admin/admin.component.ts
+++ src/app/podmap/admin/admin.component.ts
@@ -1,20 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { AngularFireAuth } from 'angularfire2/auth';
 import { Router } from '@angular/router';
+import { AngularFirestore } from 'angularfire2/firestore';
+import { PodcastSuggestion } from '../models';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'pm-admin',
   templateUrl: './admin.component.html',
   styleUrls: ['./admin.component.scss']
 })
-export class AdminComponent implements OnInit {
+export class AdminComponent {
+
+  suggestions: Observable<PodcastSuggestion[]>;
 
   constructor(
     private afAuth: AngularFireAuth,
-    private router: Router
-  ) { }
+    private router: Router,
+    private afs: AngularFirestore
+  ) {
+    this.suggestions = afs.collection<PodcastSuggestion>('suggestions').valueChanges();
+  }
 
-  ngOnInit() {
+  approve() {
+    // todo
+  }
+
+  reject() {
+    // todo
   }
 
   signOut() {
