7b345a41b094beff0e89af8f7829c9914cba838b
Jamie Perkins <jamie.perkins@gmail.com>
2019-01-07 12:51:14 -0700

    fix GA tracking

--- README.md
+++ README.md
@@ -5,13 +5,7 @@ The goals of Podmap are:
 - to help you discover where your favorite podcasts are created
 - to help you discover podcasts produced near you
 
-Podmap has a rotating set of links on the site that will feature projects of the contributors.
-
 Visit Podmap at https://podmap.org
-
-### Contributing
-
-You don't need to be a developer to contribute. More on this soon...
 
 ### Getting started
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,22 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
+declare var gtag;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
-export class AppComponent {}
+export class AppComponent {
+
+  constructor(router: Router) {
+    const navAndEvents = router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    );
+    navAndEvents.subscribe((event: NavigationEnd) => {
+      gtag('config', 'UA-116798031-2', {
+        'page_path': event.urlAfterRedirects
+      });
+    });
+  }
+}
